93ff96f4211a387539fec6fb89c7313fba366e16
Fei Yan <skyscribe.yf@gmail.com>
2014-07-27 20:14:57 +0800

    Add more checkers and dump details.

--- spotool.py
+++ spotool.py
@@ -9,25 +9,37 @@ class FBPChecker(object):
 		self._logger = logging.getLogger("FBPChecker")
 		self._logger.setLevel(logging.DEBUG)
 		self._fbp = FBPLoader(fbpFileAbsPath, fbpSheetName)
+		self._effInvalid = lambda eff: str(eff) == '' or (not str(eff).isdigit()) or int(eff) <= 1
 
 	def checkAll(self):
 		self.checkTODO()
 		self.checkConflicts()
+		self.checkForCPRIHEffortsNotGiven()
+		self.checkFTEffortsNotGiven()
 
 	def checkTODO(self):
 		self._check(['Status_TDD CPRI H'], lambda cols: cols[0] == 'no plan', 'TODO')
 
 	def checkConflicts(self):
-		self._check(['Status_TDD CPRI H', 'i_TDD CPRI H'], lambda cols: cols[0] == 'obsoleted' 
+		self._check(['Status_TDD CPRI H', 'i_TDD CPRI H'], lambda cols: cols[0] == 'obsolete' 
 			and (cols[1] == 'x' or cols[1] == 'u'), banner = "StatusConflictCheckOnCPRIH")
 
-	def _check(self, columnsForFilter, filterCriteria, banner, dumpDetails):
+	def checkForCPRIHEffortsNotGiven(self):
+		self._check(['i_TDD CPRI H', 'RealRemaining effort_TDD CPRI H'], lambda cols: cols[0] == 'x'
+			and self._effInvalid(cols[1]), banner = "CPRIHEffortsNotGiven")
+
+	def checkFTEffortsNotGiven(self):
+		self._check(['i_FT', 'Feature Team', 'RealRemaining effort_FT'], lambda cols: cols[0] == 'x'
+			and (cols[1] == 'HZ03' or cols[1] == 'HZ04')
+			and self._effInvalid(cols[2]), banner = "FTEffortsNotGiven")
+
+	def _check(self, columnsForFilter, filterCriteria, banner):
 		#Generic checker
 		self._logger.info("=================Tasks %s beg=============", banner)
 		for row in self._fbp.filterRowsByColPred(columnsForFilter, filterCriteria):
 			fid = self._fbp.getFieldForRow(row, 'Feature or Subfeature')
 			self._logger.debug("Check this feature:%s, details=<%s>", fid, ','.join(
-				['%s = %s' % (col, self._fbp.getFiledForRow(row, col)) for col in columnsForFilter]))
+				['%s = %s' % (col, self._fbp.getFieldForRow(row, col)) for col in columnsForFilter]))
 		self._logger.info("=================Tasks %s end=============", banner)
 
 
